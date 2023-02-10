4533fce64f5e7e1069b769875877327b4f272e2e
danielefongo <danielefongo@gmail.com>
2023-02-10 18:39:19 +0100

    add simple actix route

--- web/src/main.rs
+++ web/src/main.rs
@@ -1,3 +1,39 @@
-fn main() {
-    println!("Hello, world!");
+use std::fmt::Display;
+
+use actix_web::{
+    web::{self, Data},
+    App, HttpResponse, HttpServer, ResponseError,
+};
+
+#[derive(Debug)]
+pub struct Error;
+impl ResponseError for Error {}
+impl Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Error")
+    }
+}
+
+#[derive(Debug)]
+pub struct Context {}
+
+pub async fn foo(state: Data<Context>) -> HttpResponse {
+    HttpResponse::Ok()
+        .content_type("application/json")
+        .body(format!("{:?}", state))
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "debug");
+    env_logger::init();
+
+    HttpServer::new(|| {
+        App::new()
+            .app_data(Data::new(Context {}))
+            .route("/", web::get().to(foo))
+    })
+    .bind(("0.0.0.0", 8080))?
+    .run()
+    .await
 }
